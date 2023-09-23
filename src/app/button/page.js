24a9421e72ef85014b3c64ecff11a372de7465b7
Fumbi1 <olumuyiwafumbi@gmail.com>
2023-09-23 10:01:38 +0100

    Fixed bugs

--- src/app/button/page.js
+++ src/app/button/page.js
@@ -2,6 +2,13 @@ import "./btn.css";
 import Link from "next/link";
 
 const Button = ({route, closee, children}) => {
+
+  Button.defaultProps = {
+    route: null,
+    closee: null,
+    children: null,
+  };
+
   return <div>
     <div className="btn_wrapper">
         <Link href={route} onClick={closee} className="link">{children}</Link>
--- src/app/button2/page.js
+++ src/app/button2/page.js
@@ -2,6 +2,13 @@ import "./btn2.css";
 import Link from "next/link";
 
 const Button2 = ({route, children}) => {
+
+  Button2.defaultProps = {
+    route: null,
+    children: null,
+  };
+
+
   return <div>
     <div className="btn_wrapper">
         <Link href={route} className="link">{children}</Link>
